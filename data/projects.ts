d56fe2af6b73ee6716ae89d124f89ea82513f148
Cuypboy <joepcprs@gmail.com>
2026-06-16 21:11:10 +0200

    Create initial projects data

---
+++ data/projects.ts
@@ -0,0 +1,7 @@
+export const projects = [
+ { slug:'art-design', title:'Art & Design' },
+ { slug:'bilal-wahib-paradiso', title:'Bilal Wahib Paradiso' },
+ { slug:'sprintersoundsystem', title:'SprinterSoundSystem' },
+ { slug:'turndownforwhat', title:'TurnDownForWhat' },
+ { slug:'mochar', title:'MocHar' }
+];
